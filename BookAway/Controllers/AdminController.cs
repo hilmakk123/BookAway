3846a2c81de79d33a10d26c66042012d365b37c3
Hilma Kk <hilma.kk@soti.net>
2022-10-08 21:23:51 +0530

    'admin

---
+++ BookAway/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using BookAway.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+
+namespace BookAway.Controllers
+{
+    public class AdminController : Controller
+    {
+        BookAwayEntities entities = new BookAwayEntities();
+
+        // GET: HotelOwner
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoginAdminl()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginAdminl(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                bool n = entities.AdminLogins.Any(x => x.AdminUsername == login.CustUsername && x.AdminPassword == login.CustPassword);
+                if (n)
+                {
+                    FormsAuthentication.SetAuthCookie(login.CustUsername, false);
+
+
+                    return RedirectToAction("Index", "HotelOwner");
+                }
+
+                else
+                {
+                    Response.Write(@"<script>alert('Not Valid User');</script>");
+                }
+            }
+            //Response.Write(@"<script>alert('!!Not Valid User');</script>");
+
+            return View();
+        }
+        public ActionResult LogOutCust()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BookAway/Controllers/HotelOwnerController.cs
@@ -0,0 +1,49 @@
+﻿
+using BookAway.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BookAway.Controllers
+{
+    public class HotelOwnerController : Controller
+    {
+        BookAwayEntities entities = new BookAwayEntities();
+
+        // GET: HotelOwner
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoginHotel ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginHotel(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                bool n = entities.HotelOwners.Any(x => x.HOwnerUsername == login.CustUsername && x.HOwnerPassword== login.CustPassword);
+                if (n)
+                {
+                    FormsAuthentication.SetAuthCookie(login.CustUsername, false);
+
+                    
+                    return RedirectToAction("Index", "HotelOwner");
+                }
+
+                else
+                {
+                    Response.Write(@"<script>alert('Not Valid User');</script>");
+                }
+            }
+            //Response.Write(@"<script>alert('!!Not Valid User');</script>");
+
+            return View();
+        }
+    }
+}
